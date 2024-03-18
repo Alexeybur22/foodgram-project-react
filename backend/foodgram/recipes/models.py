4a3e01bb86f46504bee08b97247b44e8da2ea97f
schneidermu <schneider.mark14@gmail.com>
2024-03-18 21:31:48 +0300

    Initialize models

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Recipe(models.Model):
+    favorite = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="favorites"
+    )
+
+
+class Ingredient(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
+
+
+class RecipeIngredient(models.Model):
+    pass
+
+
+class RecipeTag(models.Model):
+    pass
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="followed"
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
+
