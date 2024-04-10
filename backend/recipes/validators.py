a72d886986fedfe86228e9263734b092ec556414
schneidermu <schneider.mark14@gmail.com>
2024-04-10 12:14:19 +0300

    delete unused

--- backend/recipes/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_nonzero(value):
-    if value == 0:
-        raise ValidationError(
-            ("Количество ингредиента %(value)s запрещено"),
-            params={"value": value},
-        )
