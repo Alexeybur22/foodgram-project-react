efbca65811ef0e61c0015e85ae0b6de99606805f
schneidermu <schneider.mark14@gmail.com>
2024-04-11 00:05:44 +0300

    omg

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -4,7 +4,8 @@ from django.forms.fields import MultipleChoiceField
 from django_filters.filters import Filter
 from recipes.models import Recipe, Tag
 
-known_tags = set(Tag.objects.values_list("slug", flat=True))
+#known_tags = set(Tag.objects.values_list("slug", flat=True))
+known_tags = ''
 TAG_CHOICES = [(tag, tag) for tag in known_tags]
 
 
